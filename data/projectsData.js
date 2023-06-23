ee2e9d0d64ca79074a1a28ae3570f951727524a6
the-intern <ostclasses@gmail.com>
2023-06-23 13:58:52 -0500

    rewrite to tailwind next js blog

---
+++ data/projectsData.js
@@ -0,0 +1,17 @@
+const projectsData = [
+  // {
+  //   title: 'A Search Engine',
+  //   description: `What if you could look up any information in the world? Webpages, images, videos
+  //   and more. Google has many features to help you find exactly what you're looking
+  //   for.`,
+  //   imgSrc: '/static/images/google.png',
+  //   href: 'https://www.google.com',
+  // },
+  {
+    title: 'Work in Progress',
+    description: `This project is in its very early stages.  More to come ...`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+]
+export default projectsData

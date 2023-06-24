8025be18cd4e28dc27e30a4154754d2adcb08774
the-intern <ostclasses@gmail.com>
2023-06-24 09:34:12 -0500

    change logo to brain svg

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -10,6 +10,7 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  // siteLogo: '/static/images/logo.png',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
